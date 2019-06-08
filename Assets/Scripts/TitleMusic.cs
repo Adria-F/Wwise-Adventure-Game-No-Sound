64804efbdef02de5b78ed25c9da8a23fe9407e14
FurryGhoul <ivandrofiak@hotmail.com>
2019-06-08 21:36:12 +0200

    Title music completed and credit loop improved

---
+++ Assets/Scripts/TitleMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMusic : MonoBehaviour
+{
+    public AudioSource source;
+
+    float duration;
+    float reset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        duration = 82;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - reset >= duration)
+        {
+            reset = Time.time;
+            source.time = 6;
+            source.Play();
+            duration = 76;
+        }
+    }
+}
