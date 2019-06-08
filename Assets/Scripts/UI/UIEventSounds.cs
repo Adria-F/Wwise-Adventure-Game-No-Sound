a82abcd7fc7b4ee1ef7603f463c1bb13a4edce9e
joseppi <josep.pi.serra@gmail.com>
2019-06-08 20:55:11 +0200

    GUI elements have audio now

--- Assets/Scripts/UI/UIEventSounds.cs
+++ Assets/Scripts/UI/UIEventSounds.cs
@@ -9,14 +9,23 @@ using UnityEngine.EventSystems;
 
 public class UIEventSounds : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
+  
     public void OnPointerDown(PointerEventData eventData)
     {
-        // Play OnPointerDownSound sound
+        // Play OnPointerDownSound sound       
+        AudioSource audio_source;
+        audio_source = GameObject.FindGameObjectWithTag("Menu/UI").GetComponent<AudioSource>();
+        audio_source.clip = Resources.Load<AudioClip>("Audio/Interface/BAS_Button_Open");
+        audio_source.Play();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         // Play OOnPointerEnterSound sound
+        AudioSource audio_source;
+        audio_source = GameObject.FindGameObjectWithTag("Menu/UI").GetComponent<AudioSource>();
+        audio_source.clip = Resources.Load<AudioClip>("Audio/Interface/BAS_Button_Enter");
+        audio_source.Play();
     }
 
     public void OnPointerExit(PointerEventData eventData)
