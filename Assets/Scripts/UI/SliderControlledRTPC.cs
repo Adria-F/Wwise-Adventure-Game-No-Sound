59578060c79ab29214688c072ac909bf8f9e5084
Adria-F <adriaferrer2898@gmail.com>
2019-06-09 15:03:50 +0200

    added sfx slider + improved script

--- Assets/Scripts/UI/SliderControlledRTPC.cs
+++ Assets/Scripts/UI/SliderControlledRTPC.cs
@@ -12,13 +12,24 @@ public class SliderControlledRTPC : MonoBehaviour
     //public AK.Wwise.RTPC RTPC;
     public AudioMixer mixer;
     public string volumeName;
+    public float minVolume = 0.0f;
+
+    float initialVolume = 0.0f;
+
+    private void Start()
+    {
+        mixer.GetFloat(volumeName, out initialVolume);
+    }
 
     public void SetRTPC(float value)
     {
         if (Menu.isOpen)
         {
             //RTPC.SetGlobalValue(value);
-            mixer.SetFloat(volumeName, value-80);
+            float normal = Mathf.InverseLerp(0f, 100f, value);
+            float volume = Mathf.Lerp(minVolume, initialVolume, normal);
+
+            mixer.SetFloat(volumeName, volume);
         }
     }
 
